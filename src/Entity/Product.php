cd53c760ceac3484daab702914bccfd7184abe48
mak-1920 <roma.ivanov.1920@mail.ru>
2022-01-18 10:42:11 +0300

    initial project and create migrations

---
+++ src/Entity/Product.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 'false'])]
+    private $discontinued;
+
+    #[ORM\OneToMany(mappedBy: 'Product', targetEntity: Import::class, orphanRemoval: true)]
+    private $imports;
+
+    public function __construct()
+    {
+        $this->imports = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDiscontinued(): ?bool
+    {
+        return $this->discontinued;
+    }
+
+    public function setDiscontinued(bool $discontinued): self
+    {
+        $this->discontinued = $discontinued;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Import[]
+     */
+    public function getImports(): Collection
+    {
+        return $this->imports;
+    }
+
+    public function addImport(Import $import): self
+    {
+        if (!$this->imports->contains($import)) {
+            $this->imports[] = $import;
+            $import->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImport(Import $import): self
+    {
+        if ($this->imports->removeElement($import)) {
+            // set the owning side to null (unless already changed)
+            if ($import->getProduct() === $this) {
+                $import->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+}
