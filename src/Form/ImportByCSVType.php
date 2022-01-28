f3c7ed9f0c8c6d7c342dec3be8d1db59209d2f79
mak-1920 <roma.ivanov.1920@mail.ru>
2022-01-28 11:55:31 +0300

    add webpack-encore

--- src/Form/ImportByCSVType.php
+++ src/Form/ImportByCSVType.php
@@ -6,12 +6,11 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ImportByCSVType extends AbstractType
@@ -19,36 +18,6 @@ class ImportByCSVType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('delimiter', TextType::class, [
-                'required' => true,
-                'data' => ',',
-                'constraints' => [
-                    new NotBlank(),
-                    new Length(min: 1, max: 1),
-                ],
-                'attr' => [
-                    'minlength' => 1,
-                    'maxlength' => 1,
-                ],
-            ])
-            ->add('enclosure', TextType::class, [
-                'data' => '',
-                'constraints' => [
-                    new Length(min: 0, max: 1),
-                ],
-                'attr' => [
-                    'maxlength' => 1,
-                ],
-            ])
-            ->add('escape', TextType::class, [
-                'data' => '',
-                'constraints' => [
-                    new Length(min: 0, max: 1),
-                ],
-                'attr' => [
-                    'maxlength' => 1,
-                ],
-            ])
             ->add('file', FileType::class, [
                 'multiple' => true,
                 'required' => true,
@@ -56,14 +25,14 @@ class ImportByCSVType extends AbstractType
                     new NotBlank(),
                 ],
             ])
-            ->add('haveHeader', CheckboxType::class, [
-                'mapped' => false,
-                'required' => false,
-                'data' => true,
-            ])
             ->add('testmode', CheckboxType::class, [
                 'mapped' => false,
                 'required' => false,
+            ])
+            ->add('csvSettings', CollectionType::class, [
+                'allow_add' => true,
+                'entry_type' => CsvSettingsType::class,
+                'label' => false,
             ])
             ->add('import', SubmitType::class, [
             ])
